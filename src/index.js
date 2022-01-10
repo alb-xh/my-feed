76d8c3a1db885023bb0e0d8efae92a918a67d248
alb-xh <albano.xhafaj10@gmail.com>
2022-01-10 20:30:16 +0500

    Setup the server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(
+  process.env.SERVER_PORT,
+  process.env.SERVER_HOST,
+  () => {
+    console.log('Server is running');
+  }
+);
