85215bb372a32a787394ce6c23c0a802c28b1ad5
alb-xh <albano.xhafaj10@gmail.com>
2022-01-11 17:59:23 +0500

    refactored app

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,7 @@
+const path = require('path');
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+app.use(express.static('src/server/public'));
 
 module.exports = app;
---
+++ src/server/tests/server.test.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+const supertest = require('supertest');
+const server = require('../index');
+
+
+const request = supertest(server);
+
+describe('server', () => {
+  describe('/', () => {
+    it('responds with index.html file', async () => {
+      const response = await request.get('/');
+
+      const htmlFilePath = path.join(__dirname, '..', 'public', 'index.html')
+      const expectedFile = await fs.readFile(htmlFilePath, 'utf-8');
+
+      expect(response.status).toBe(200)
+      expect(response.text).toStrictEqual(expectedFile);
+    });
+  });
+});
