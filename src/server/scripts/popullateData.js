8d1143e73a5ddd110416b8c890951b59f37a3af1
alb-xh <albano.xhafaj10@gmail.com>
2022-02-10 17:59:18 +0500

    Fix

--- src/server/scripts/popullateData.js
+++ src/server/scripts/popullateData.js
@@ -3,18 +3,33 @@ const fs = require('fs/promises');
 
 const DATA_FOLDER = path.join(__dirname, '..', 'data');
 
-const addFile = async (filename, data) => {
-  const filePath = path.join(DATA_FOLDER, filename);
-  await fs.writeFile(filePath, JSON.stringify(data));
-};
+const folder = (path) => ({
+  exists: async () => {
+    try {
+      await fs.access(path);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  remove: (recursive = true) => fs.rm(path, { recursive }),
+  create: () => fs.mkdir(path),
+});
+
+const file = (path) => ({
+  create: (data) => fs.writeFile(path, JSON.stringify(data)),
+});
 
 const popullateData = async () => {
-  try {
-    await addFile('users.json', []);
-    await addFile('sessions.json', {});
-  } catch (err) {
-    console.log(err);
-  }
+  const dataFolder = folder(DATA_FOLDER);
+  const usersFile = file(path.join(DATA_FOLDER, 'users.json'));
+  const sessionsFile = file(path.join(DATA_FOLDER, 'sessions.json'));
+
+  if (await dataFolder.exists()) await dataFolder.remove();
+
+  await dataFolder.create();
+  await usersFile.create([]);
+  await sessionsFile.create([]);
 };
 
 popullateData();
